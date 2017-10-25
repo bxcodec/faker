dccf14bf78e587e454f2e29e047b51eff815fe62
agoalofalife <agoalofalife@gmail.com>
2017-10-25 07:16:57 +0300

    fix variable internet in struct Internet

--- internet.go
+++ internet.go
@@ -50,10 +50,10 @@ type Networker interface {
 
 type Internet struct{}
 
-func (i Internet) Email() string {
+func (internet Internet) Email() string {
 	return randomString(7) + "@" + randomString(5) + "." + randomElementFromSliceString(tld)
 }
-func (i Internet) MacAddress() string {
+func (internet Internet) MacAddress() string {
 	r := rand.New(src)
 	ip := make([]byte, 6)
 	for i := 0; i < 6; i++ {
@@ -62,22 +62,22 @@ func (i Internet) MacAddress() string {
 
 	return net.HardwareAddr(ip).String()
 }
-func (i Internet) DomainName() string {
+func (internet Internet) DomainName() string {
 	return randomString(7) + "." + randomElementFromSliceString(tld)
 }
-func (i Internet) Url() string {
+func (internet Internet) Url() string {
 	format := randomElementFromSliceString(urlFormats)
 	countVerbs := strings.Count(format, "%s")
 	if countVerbs == 1 {
-		return fmt.Sprintf(format, i.DomainName())
+		return fmt.Sprintf(format, internet.DomainName())
 	} else {
-		return fmt.Sprintf(format, i.DomainName(), i.UserName())
+		return fmt.Sprintf(format, internet.DomainName(), internet.UserName())
 	}
 }
-func (i Internet) UserName() string {
+func (internet Internet) UserName() string {
 	return randomString(7)
 }
-func (i Internet) Ipv4() string {
+func (internet Internet) Ipv4() string {
 	r := rand.New(src)
 	size := 4
 	ip := make([]byte, size)
@@ -86,7 +86,7 @@ func (i Internet) Ipv4() string {
 	}
 	return net.IP(ip).To4().String()
 }
-func (i Internet) Ipv6() string {
+func (internet Internet) Ipv6() string {
 	r := rand.New(src)
 	size := 16
 	ip := make([]byte, size)
