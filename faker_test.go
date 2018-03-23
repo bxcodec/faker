807f0dad795052ceb0d76885165143974c2d529b
Iman Tumorang <bxcodec@users.noreply.github.com>
2018-03-23 15:38:06 +0700

    add test

--- faker_test.go
+++ faker_test.go
@@ -9,13 +9,20 @@ import (
 )
 
 type SomeStruct struct {
-	Inta             int
-	Int8             int8
-	Int16            int16
-	Int32            int32
-	Int64            int64
-	Float32          float32
-	Float64          float64
+	Inta    int
+	Int8    int8
+	Int16   int16
+	Int32   int32
+	Int64   int64
+	Float32 float32
+	Float64 float64
+
+	UInta  uint
+	UInt8  uint8
+	UInt16 uint16
+	UInt32 uint32
+	UInt64 uint64
+
 	Latitude         float32 `faker:"lat"`
 	LATITUDE         float64 `faker:"lat"`
 	Long             float32 `faker:"long"`
@@ -39,6 +46,10 @@ type SomeStruct struct {
 	Struct           AStruct
 	Time             time.Time
 	Stime            []time.Time
+
+	MapStringString        map[string]string
+	MapStringStruct        map[string]AStruct
+	MapStringStructPointer map[string]*AStruct
 }
 type AStruct struct {
 	Number        int64
@@ -173,6 +184,16 @@ func TestFakerData(t *testing.T) {
 	// Example Result :
 	// {Int:8906957488773767119 Int8:6 Int16:14 Int32:391219825 Int64:2374447092794071106 String:poraKzAxVbWVkMkpcZCcWlYMd Bool:false SString:[MehdV aVotHsi] SInt:[528955241289647236 7620047312653801973 2774096449863851732] SInt8:[122 -92 -92] SInt16:[15679 -19444 -30246] SInt32:[1146660378 946021799 852909987] SInt64:[6079203475736033758 6913211867841842836 3269201978513619428] SFloat32:[0.019562425 0.12729558 0.36450312] SFloat64:[0.7825838989890364 0.9732903338838912 0.8316541489234004] SBool:[true false true] Struct:{Number:7693944638490551161 Height:6513508020379591917}}
 
+}
+
+func TestUnsupertedMapStringInterface(t *testing.T) {
+	type Sample struct {
+		Map map[string]interface{}
+	}
+	sample := new(Sample)
+	if err := FakeData(sample); err == nil {
+		t.Error("Expected Got Error. But got nil")
+	}
 }
 
 func TestSetDataIfArgumentNotPtr(t *testing.T) {
