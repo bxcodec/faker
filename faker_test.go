7b6a0771281ef21bbaf4fc3a1e292b63f6c2a9f8
Dmitry Yu Okunev <xaionaro@dx.center>
2020-06-06 17:24:47 +0000

    go fmt
    Signed-off-by: Dmitry Yu Okunev <xaionaro@dx.center>

--- faker_test.go
+++ faker_test.go
@@ -972,7 +972,7 @@ func TestExtend(t *testing.T) {
 	t.Run("test-with-custom-slice-type", func(t *testing.T) {
 		a := CustomThatUsesSlice{}
 		err := AddProvider("custom-type-over-slice", func(v reflect.Value) (interface{}, error) {
-			return []byte{0,1,2,3,4}, nil
+			return []byte{0, 1, 2, 3, 4}, nil
 		})
 
 		if err != nil {
@@ -985,7 +985,7 @@ func TestExtend(t *testing.T) {
 			t.Error("Expected Not Error, But Got: ", err)
 		}
 
-		if reflect.DeepEqual(a.UUID, []byte{0,1,2,3,4}) {
+		if reflect.DeepEqual(a.UUID, []byte{0, 1, 2, 3, 4}) {
 			t.Error("UUID should equal test value")
 		}
 	})
