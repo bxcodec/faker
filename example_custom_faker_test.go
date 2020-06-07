cca5c35820b9b648e73f06f64baf536b14307824
altair <andrew-werdna@users.noreply.github.com>
2020-06-07 09:58:16 -0500

    finish fixing merge conflicts

--- example_custom_faker_test.go
+++ example_custom_faker_test.go
@@ -42,11 +42,7 @@ func CustomGenerator() {
 	})
 
 	_ = faker.AddProvider("customUUID", func(v reflect.Value) (interface{}, error) {
-<<<<<<< HEAD
 		s := CustomUUID{
-=======
-		s := []byte{
->>>>>>> origin/master
 			0, 8, 7, 2, 3,
 		}
 		return s, nil
