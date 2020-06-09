c495e5558483ebe4b68949cbd6bbc896bb31863b
Andrew Brown <8261769+andrew-werdna@users.noreply.github.com>
2020-06-09 00:07:34 -0500

    update examples

--- example_with_tags_test.go
+++ example_with_tags_test.go
@@ -54,6 +54,15 @@ type SomeStructWithTags struct {
 	AccountID          int     `faker:"oneof: 15, 27, 61"`                     // use commas to separate the values for now. Future support for other separator characters may be added
 	Price32            float32 `faker:"oneof: 4.95, 9.99, 31997.97"`
 	Price64            float64 `faker:"oneof: 47463.9463525, 993747.95662529, 11131997.978767990"`
+	NumS64             int64   `faker:"oneof: 1, 2"`
+	NumS32             int32   `faker:"oneof: 3, 4"`
+	NumS16             int16   `faker:"oneof: 5, 6"`
+	NumS8              int8    `faker:"oneof: 7, 8"`
+	NumU64             uint64  `faker:"oneof: 9, 10"`
+	NumU32             uint32  `faker:"oneof: 11, 12"`
+	NumU16             uint16  `faker:"oneof: 13, 14"`
+	NumU8              uint8   `faker:"oneof: 15, 16"`
+	NumU               uint    `faker:"oneof: 17, 18"`
 }
 
 func Example_withTags() {
@@ -112,6 +121,15 @@ func Example_withTags() {
 			AccountID: 61,
 			Price32: 4.95,
 			Price64: 993747.95662529
+			NumS64:	1
+			NumS32:	3
+			NumS16:	5
+			NumS8:	7
+			NumU64:	9
+			NumU32:	11
+			NumU16:	13
+			NumU8:	15
+			NumU:	17
 			Skip:
 		}
 	*/
